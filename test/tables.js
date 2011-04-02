73e746e3518a5a20ebe678aca584ac47636ad903
Dan Stocker <dan@kwaia.com>
2011-04-02 15:50:23 +0200

    test tables prepared for table module

--- test/tables.js
+++ test/tables.js
@@ -3,28 +3,29 @@
 ////////////////////////////////////////////////////////////////////////////////
 /*global jOrder */
 
-jOrder.testing = function (testing) {
+jOrder.testing = function (testing, constants) {
 	// table with 77 rows and 5 columns
-	testing.table77 = new jOrder.table(jOrder.testing.json77 || [])
-		.index('id', ['ID'], { ordered: true, type: jOrder.number })
+	testing.table77 = jOrder(testing.json77 || [])
+		.index('id', ['ID'], { ordered: true, type: constants.number })
 		.index('id_nosort', ['ID'])
-		.index('group', ['GroupID'], { ordered: true, grouped: true, type: jOrder.number })
-		.index('total', ['Total'], { ordered: true, grouped: true, type: jOrder.number })
+		.index('group', ['GroupID'], { ordered: true, grouped: true, type: constants.number })
+		.index('total', ['Total'], { ordered: true, grouped: true, type: constants.number })
 		.index('date', ['StartDate'], { ordered: true, grouped: true })
 		.index('signature', ['Total', 'Currency'], { ordered: true, grouped: true });
 
 	// unindexed version of 77-row table
-	testing.table77n = new jOrder.table(jOrder.testing.json77 || []);
+	testing.table77n = jOrder(testing.json77 || []);
 
 	// table with 1000 rows and 2 columns
-	testing.table1000 = new jOrder.table(jOrder.testing.json1000 || [])
-		.index('id', ['id'], { ordered: true, type: jOrder.number })
+	testing.table1000 = jOrder(testing.json1000 || [])
+		.index('id', ['id'], { ordered: true, type: constants.number })
 		.index('name', ['name'], { ordered: true, grouped: true })
-		.index('fulltext', ['name'], { ordered: true, grouped: true, type: jOrder.text });
+		.index('fulltext', ['name'], { ordered: true, grouped: true, type: constants.text });
 	
 	// unindexed version of 77-row table
-	testing.table1000n = new jOrder.table(jOrder.testing.json1000 || []);
+	testing.table1000n = jOrder(testing.json1000 || []);
 
 	return testing;
-}(jOrder.testing || {});
+}(jOrder.testing,
+	jOrder.constants);
 
