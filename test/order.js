856bac65e18a9226477d32277d099bdaa12160a6
Dan Stocker <dan@kwaia.com>
2011-04-05 22:52:37 +0200

    order modification test cases added

--- test/order.js
+++ test/order.js
@@ -8,14 +8,14 @@ jOrder.testing = function (testing, constants, jOrder) {
 	testing.order = function () {
 		module("Order");
 
-		var 
+		var
 		
 		// test indexes
 		string = jOrder.index(testing.jsonX, ['author'], {ordered: true}),
-		string_multi = jOrder.index(testing.jsonX, ['author', 'volumes']),
-		number = jOrder.index(testing.jsonX, ['volumes'], {type: jOrder.number, grouped: true}),
-		array = jOrder.index(testing.jsonX, ['data'], {type: jOrder.array, grouped: true}),
-		text = jOrder.index(testing.jsonX, ['title'], {type: jOrder.text, grouped: true});
+		//string_multi = jOrder.index(testing.jsonX, ['author', 'volumes']),
+		number = jOrder.index(testing.jsonX, ['volumes'], {type: jOrder.number, grouped: true, ordered: true});
+		//array = jOrder.index(testing.jsonX, ['data'], {type: jOrder.array, grouped: true}),
+		//text = jOrder.index(testing.jsonX, ['title'], {type: jOrder.text, grouped: true});
 		
 		test("Binary search", function () {
 			// author:"Milne" is at index 1
@@ -35,6 +35,74 @@ jOrder.testing = function (testing, constants, jOrder) {
 			equal(string.bsearch("Asim", constants.end), -1, "First item 'Asimov' is unsuitable as end of inerval");
 			equal(string.bsearch("Tol", constants.start), 2, "Last item 'Tolkien' is OK as interval start (interval length = 1)");
 			equal(string.bsearch("Tol", constants.end), 1, "Last item 'Tolkien' is OK as interval end");
+		});
+		
+		test("Modifying order", function () {
+			var expected;
+			
+			//////////////////////////////
+			// unique index (string type)
+
+			// first element
+			expected = [
+				{key: 'Tolkien', rowId: 0}
+			];
+			string
+				.unbuild()
+				.add(testing.jsonX[0], 0, false);
+			deepEqual(string.order(), expected, "Adding FIRST item to UNIQUE order");
+
+			// second element
+			expected = [
+				{key: 'Milne', rowId: 1},
+				{key: 'Tolkien', rowId: 0}
+			];
+			string
+				.add(testing.jsonX[1], 1, false)
+				.reorder();
+			deepEqual(string.order(), expected, "Adding SECOND item to UNIQUE order, then REORDERing");
+
+			//////////////////////////////
+			// grouped index (numeric type)
+			
+			// first element
+			expected = [
+				{key: 1, rowId: 1}
+			];
+			number
+				.unbuild()
+				.add(testing.jsonX[1], 1, false);
+			deepEqual(number.order(), expected, "Adding FIRST item to GROUPED order");
+			
+			// second element
+			expected = [
+				{key: 1, rowId: 1},
+				{key: 3, rowId: 0}
+			];
+			number
+				.add(testing.jsonX[0], 0, false)
+				.reorder();
+			deepEqual(number.order(), expected, "Adding SECOND item to GROUPED order, then REORDERing");
+
+			// third element
+			expected = [
+				{key: 1, rowId: 1},
+				{key: 1, rowId: 2},
+				{key: 3, rowId: 0}
+			];
+			number
+				.add(testing.jsonX[2], 2, false)
+				.reorder();
+			deepEqual(number.order(), expected, "Adding THIRD item to GROUPED order, then REORDERing");
+			
+			// removing first element
+			expected = [
+				{key: 1, rowId: 2},
+				{key: 3, rowId: 0}
+			];
+			number
+				.remove(testing.jsonX[1], 1);
+			deepEqual(number.order(), expected, "Removing FIRST item from GROUPED order, then REORDERing");
 		});
 	}();
 	
