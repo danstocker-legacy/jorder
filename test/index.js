397eafb1fc988b5e45b064c944881f24182637c9
Dan Stocker <dan@kwaia.com>
2011-04-08 20:44:02 +0200

    additional general index test cases

--- test/index.js
+++ test/index.js
@@ -1,19 +1,41 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Unit tests for jOrder index
 ////////////////////////////////////////////////////////////////////////////////
-/*global console, jOrder, module, test, ok, equal, deepEqual, raises */
+/*global console, jOrder, module, test, ok, equal, notEqual, deepEqual, raises */
 
-jOrder.testing = function (testing, jOrder) {
+jOrder.testing = function (testing, constants, jOrder) {
 	// Data integrity tests
 	testing.index = function () {
 		module("Indexing");
 
+		var index = jOrder.index(testing.json77, ['ID'], {ordered: true, build: false});
+
+		// detects whether an object has any own properties
+		function empty(obj) {
+			var i;
+			for (i in obj) {
+				if (obj.hasOwnProperty(i)) {
+					return false;
+				}
+			}
+			return true;
+		}
+		
 		test("Initialization", function () {
-			deepEqual(jOrder.index(testing.json77, ['ID'], {build: false}).flat(), {}, "Building index on initialization can be turned off");
+			ok(index.order().length === 0 && empty(index.flat()), "Building index on initialization can be turned off");
+			ok(index.rebuild().order().length > 0 && !empty(index.flat()), "Rebuilding index on demand");
+			ok(index.unbuild().order().length === 0 && empty(index.flat()), "Clearing index on demand");
+		});
+		
+		test("Getters", function () {
+			ok(!index.grouped(), "Grouped state");
+			ok(index.ordered(), "Grouped state");
+			equal(index.type(), constants.string, "Index type");
 		});
 	}();
 	
 	return testing;
 }(jOrder.testing,
+	jOrder.constants,
 	jOrder);
 
