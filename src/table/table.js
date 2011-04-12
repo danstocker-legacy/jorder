3ed37ef68c03c4dd157a32c40fd212441b8871b4
Dan Stocker <dan@kwaia.com>
2011-04-12 09:07:17 +0200

    default selectors factored out to separate file

--- src/table/table.js
+++ src/table/table.js
@@ -3,53 +3,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 /*global jOrder */
 
-jOrder.table = function ($core, $constants, $logging, $indexes) {
-	var
-	
-	// selectors for table.filter()
-	selectors = {
-		// selects exact matches
-		// handles multiple conditions
-		exact: function (row, data) {
-			var match = false,
-					partial, condition,
-					i, field;
-			for (i = 0; i < data.conditions.length; i++) {
-				partial = true;
-				condition = data.conditions[i];
-				for (field in condition) {
-					if (condition.hasOwnProperty(field)) {
-						partial &= (condition[field] === row[field]);
-						if (!partial) {
-							break;
-						}
-					}
-				}
-				match |= partial;
-				if (match) {
-					break;
-				}
-			}
-			return match;
-		},
-		
-		// selectes start of word matches
-		// only first condition is processed
-		startof: function (row, data) {
-			var kv = $core.split(data.conditions[0]);
-			return row[kv.keys[0]].indexOf(kv.values[0]) === 0;
-		},
-		
-		// selects range of values
-		// only first condition is processed
-		range: function (row, data) {
-			var kv = $core.split(data.conditions[0]),
-					bounds = kv.values[0],
-					field = kv.keys[0];
-			return bounds.lower <= row[field] && bounds.upper > row[field];
-		}
-	};
-	
+jOrder.table = function ($core, $constants, $logging, $indexes, $selectors) {
 	// indexed table object
 	// - data: json table the table object is based on
 	// - options:
@@ -245,14 +199,14 @@ jOrder.table = function ($core, $constants, $logging, $indexes) {
 					// obtaining suitable selector
 					switch (options.mode) {
 					case $constants.range:
-						selector = selectors.range;
+						selector = $selectors.range;
 						break;
 					case $constants.startof:
-						selector = selectors.startof;
+						selector = $selectors.startof;
 						break;
 					default:
 					case $constants.exact:
-						selector = selectors.exact;
+						selector = $selectors.exact;
 						break;
 					}
 					
@@ -462,5 +416,6 @@ jOrder.table = function ($core, $constants, $logging, $indexes) {
 }(jOrder.core,
 	jOrder.constants,
 	jOrder.logging,
-	jOrder.indexes);
+	jOrder.indexes,
+	jOrder.selectors);
 
