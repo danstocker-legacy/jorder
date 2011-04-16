1f8be8cc88b816b26e8f87c3eb261b77ecb95ff7
Dan Stocker <dan@kwaia.com>
2011-04-16 10:08:38 +0200

    benchmarking refactored

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,307 +1,136 @@
-/*jslint onevar:false, forin:true */
-/*global $, jOrder */
-
-// env variables
-var cycles = 10;
-var categories =
-{
-	'exact_search77': "Searching for exact matches ('GroupID' being either 107 or 185)",
-	'composite_search77': "Searching for exact matches (where 'Total' = 8 and 'Currency' = 'USD')",
-	'range_search77': "Range search ('Total' between 11 and 15)",
-	'exact_search1000': "Searching for exact matches ('id' being either 107 or 115)",
-	'range_search1000': "Range search ('id' between 203 and 315)",
-	'range_search_page1000': "Range search with page result ('id' between 203 and 315; hits #20 to #40)",
-	'freetext_search1000': "Free text search (rows with 'name' field starting with \"con\")",
-	'sorting77': "Sorting by 'ID'",
-	'sorting1000': "Sorting by 'name'",
-	'sorting_page1000': "Sorting by 'name' (first 20 hits)",
-	'aggregate77': "Grouping by 'GroupID'",
-	'array': "Concatenating an array to itself multiple times"
-};
-
-// ad hoc table builder
-function build_table(dest, data)
-{
-	dest.empty();
-	
-	// head
-	var colgroup = $('<colgroup />');
-	dest.append(colgroup);
-	var thead = $('<thead><tr></tr></thead>');
-	dest.append(thead);
-	for (var idx in data)
-	{
-		for (var key in data[idx])
-		{
-			colgroup.append('<col />');
-			thead.append('<th>' + key + '</th>');
-		}
-		break;
-	}
-	
-	// body
-	var tbody = $('<tbody></tbody>');
-	dest.append(tbody);
-	for (idx in data)
-	{
-		var tr = $('<tr />');
-		tbody.append(tr);
-		for (key in data[idx])
-		{
-			var td = $('<td />');
-			tr.append(td);
-			td.append(data[idx][key]);
-		}
-	}
-}
-
-// function that registers a benchmark function on the page
-function register_benchmark(table, category, description, callback, options)
-{
-	options = options || {};
-	
-	// adding tbody
-	var tbody = $('#' + table + ' > tbody.' + category);
-	if (!tbody.length)
-	{
-		tbody = $('<tbody class="' + category + '"/>');
-		$('#' + table).append(tbody);
-		tbody.append('<tr><th colspan="3">' + categories[category] + '</th></tr>');
-	}
-	
-	// adding row for benchmark
-	var tr = $('<tr />');
-	tbody.append(tr);
-	if (options.isreference) {
-		tr.addClass('reference');
-	}
-	var button = $('<input type="button" value="&#8594;" />');
-	tr.append('<td>' + description + '</td>');
-	var td_button = $('<td></td>');
-	tr.append(td_button);
-	td_button.append(button);
-	var timecell = $('<td />');
-	tr.append(timecell);
-	var arrowcell = $('<td class="arrow" />');
-	tr.append(arrowcell);
-	
-	var arrowspan = $('#arrow');
-	if (!arrowspan.length)
-	{
-		arrowspan = $('<span id="arrow">&#8594;</span>');
-		arrowspan.hide();
-		arrowcell.append(arrowspan);
-	}
-	
-	button.click(function ()
-	{
-		var result;
-		var idx;
-
-		// parameters
-		var cycles = $('#count').val();
-		var timeout = $('#timeout').val();
-		var unit = $('#units').attr('checked') ? " ms" : "";
-		var estimate = $('#estimate').attr('checked');
-		
-		// run benchmark n times
-		var start = new Date();
-		for (idx = 0; idx < cycles; idx++)
-		{
-			result = callback();
-			if (timeout < new Date() - start) {
-				break;
-			}
-		}
-		var end = new Date();
-		
-		// handle timeout
-		if (idx < cycles)
-		{
-			timecell.text(estimate ?
-				String(Math.floor(timeout * cycles / idx)) + unit :
-				"timeout (" + Math.floor(100 * idx / cycles) + "%)");
-			arrowspan.hide();
-			$('#result').empty();
-			return;
-		}
-		
-		// display measured time & results
-		timecell.text(String(end - start) + unit);
-		arrowcell.append(arrowspan);
-		arrowspan.show();
-
-		$('#result').css('top', tbody.offset().top);		
-		build_table($('#result'), options.lengthonly ? [{ length: result.length }] : result);
-	});
-}
+////////////////////////////////////////////////////////////////////////////////
+// jOrder performance tests
+////////////////////////////////////////////////////////////////////////////////
+/*global jQuery, jOrder, jOB */
 
 // registering benchmarks on document ready
-$(function ()
-{
-	// Exact search on 77 rows
-	
-	register_benchmark('small', 'exact_search77', "jOrder.table.where()", function ()
-	{
-		return jOrder.testing.table77.where([{ 'GroupID': 107 }, { 'GroupID': 185 }], { renumber: true });
-	});
-
-	register_benchmark('small', 'exact_search77', "Row by row iteration", function ()
-	{
-		return jOrder.testing.table77n.filter(function (row)
-		{
-			return row.GroupID === 107 || row.GroupID === 185;
+(function ($, jOrder, jOB) {
+	$(function () {
+		// Exact search on 77 rows
+		jOB.benchmark("Searching for exact matches ('GroupID' being either 107 or 185)");
+		jOB.test("jOrder.table.where()", function () {
+			return jOrder.testing.table77.where([{ 'GroupID': 107 }, { 'GroupID': 185 }], { renumber: true });
 		});
-	}, { isreference: true });
-
-	// Exact search on composite index search
+		jOB.test("Row by row iteration", function () {
+			return jOrder.testing.table77n.filter(function (row) {
+				return row.GroupID === 107 || row.GroupID === 185;
+			}, {renumber: true});
+		}, { isreference: true });
 	
-	register_benchmark('small', 'composite_search77', "jOrder.table.where()", function ()
-	{
-		return jOrder.testing.table77.where([{ 'Currency': 'USD', 'Total': 8 }], { renumber: true });
-	});
-	
-	register_benchmark('small', 'composite_search77', "Row by row iteration", function ()
-	{
-		return jOrder.testing.table77n.filter(function (row)
-		{
-			return row.Currency === 'USD' && row.Total === 8;
+		// Exact search on composite index search
+		jOB.benchmark("Searching for exact matches (where 'Total' = 8 and 'Currency' = 'USD')");
+		jOB.test("jOrder.table.where()", function () {
+			return jOrder.testing.table77.where([{ 'Currency': 'USD', 'Total': 8 }], { renumber: true });
 		});
-	}, { isreference: true });
-
-	// Exact search on 1000 rows
-
-	register_benchmark('large', 'exact_search1000', "jOrder.table.where()", function ()
-	{
-		return jOrder.testing.table1000.where([{ 'id': 107 }, { 'id': 115 }]);
-	});
-
-	register_benchmark('large', 'exact_search1000', "Row by row iteration", function ()
-	{
-		return jOrder.testing.table1000n.filter(function (row)
-		{
-			return row.id === 107 || row.id === 115;
+		jOB.test("Row by row iteration", function () {
+			return jOrder.testing.table77n.filter(function (row) {
+				return row.Currency === 'USD' && row.Total === 8;
+			}, {renumber: true});
+		}, { isreference: true });
+	
+		// Exact search on 1000 rows
+		jOB.benchmark("Searching for exact matches ('id' being either 107 or 115)");
+		jOB.test("jOrder.table.where()", function () {
+			return jOrder.testing.table1000.where([{ 'id': 107 }, { 'id': 115 }], {renumber: true});
 		});
-	}, { isreference: true });
-
-	// Range search on 77 rows
+		jOB.test("Row by row iteration", function () {
+			return jOrder.testing.table1000n.filter(function (row) {
+				return row.id === 107 || row.id === 115;
+			}, {renumber: true});
+		}, { isreference: true });
 	
-	register_benchmark('small', 'range_search77', "jOrder.table.where()", function ()
-	{
-		return jOrder.testing.table77.where([{ 'Total': { lower: 11, upper: 15 } }], { mode: jOrder.range });
-	});
-
-	register_benchmark('small', 'range_search77', "Row by row iteration", function ()
-	{
-		return jOrder.testing.table77n.filter(function (row)
-		{
-			return row.Total >= 11 && row.Total <= 15;
+		// Range search on 77 rows
+		jOB.benchmark("Range search ('Total' between 11 and 15)");	
+		jOB.test("jOrder.table.where()", function () {
+			return jOrder.testing.table77.where([{ 'Total': { lower: 11, upper: 15 } }], { mode: jOrder.range, renumber: true });
 		});
-	}, { isreference: true });
-	
-	// Range search on 1000 rows
-
-	register_benchmark('large', 'range_search1000', "jOrder.table.where()", function ()
-	{
-		return jOrder.testing.table1000.where([{ 'id': { lower: 203, upper: 315 } }], { mode: jOrder.range, renumber: true, limit: 1000 });
-	});
-
-	register_benchmark('large', 'range_search1000', "Row by row iteration", function ()
-	{
-		return jOrder.testing.table1000n.filter(function (row)
-		{
-			return row.id >= 203 && row.id <= 315;
+		jOB.test("Row by row iteration", function () {
+			return jOrder.testing.table77n.filter(function (row) {
+				return row.Total >= 11 && row.Total <= 15;
+			}, {renumber: true});
+		}, { isreference: true });
+		
+		// Range search on 1000 rows
+		jOB.benchmark("Range search ('id' between 203 and 315)");
+		jOB.test("jOrder.table.where()", function () {
+			return jOrder.testing.table1000.where([{ 'id': { lower: 203, upper: 315 } }], { mode: jOrder.range, renumber: true, limit: 1000 });
 		});
-	}, { isreference: true });
-
-	// Range search on 1000 rows with limit
+		jOB.test("Row by row iteration", function () {
+			return jOrder.testing.table1000n.filter(function (row) {
+				return row.id >= 203 && row.id <= 315;
+			}, {renumber: true});
+		}, { isreference: true });
 	
-	register_benchmark('large', 'range_search_page1000', "jOrder.table.where()", function ()
-	{
-		return jOrder.testing.table1000.where([{ 'id': { lower: 203, upper: 315 } }],
-		{
-			mode: jOrder.range,
-			renumber: true,
-			offset: 20,
-			limit: 20
+		// Range search on 1000 rows with limit
+		jOB.benchmark("Range search with page result ('id' between 203 and 315; hits #20 to #40)");
+		jOB.test("jOrder.table.where()", function () {
+			return jOrder.testing.table1000.where([{ 'id': { lower: 203, upper: 315 } }], {
+				mode: jOrder.range,
+				renumber: true,
+				offset: 20,
+				limit: 20
+			}, {renumber: true});
 		});
-	});
-
-	// Freetext search on 1000 rows
 	
-	register_benchmark('large', 'freetext_search1000', "jOrder.table.where()", function ()
-	{
-		return jOrder.testing.table1000.where([{ 'name': 'con' }], { mode: jOrder.startof, indexName: 'fulltext', limit: 1000 });
-	});
-
-	register_benchmark('large', 'freetext_search1000', "Row by row iteration", function ()
-	{
-		return jOrder.testing.table1000n.filter(function (row)
-		{
-			return null !== row.name.match(/\bcon/i);
+		// Freetext search on 1000 rows
+		jOB.benchmark("Free text search (rows with 'name' field starting with \"con\")");
+		jOB.test("jOrder.table.where()", function () {
+			return jOrder.testing.table1000.where([{ 'name': 'con' }], { mode: jOrder.startof, indexName: 'fulltext', limit: 1000, renumber: true });
 		});
-	}, { isreference: true });
-	
-	// Sorting on 77 rows
-	
-	register_benchmark('small', 'sorting77', "jOrder.table.orderby()", function ()
-	{
-		return jOrder.testing.table77.orderby(['ID'], jOrder.asc, { indexName: 'id' });
-	});
-
-	register_benchmark('small', 'sorting77', "Row by row iteration", function ()
-	{
-		return jOrder.shallow(jOrder.testing.table77.flat()).sort(function (a, b)
-		{
-			return a.ID > b.ID ? 1 : a.ID < b.ID ? -1 : 0;
+		jOB.test("Row by row iteration", function () {
+			return jOrder.testing.table1000n.filter(function (row) {
+				return null !== row.name.match(/\bcon/i);
+			}, {renumber: true});
+		}, { isreference: true });
+		
+		// Sorting on 77 rows
+		jOB.benchmark("Sorting by 'ID'");
+		jOB.test("jOrder.table.orderby()", function () {
+			return jOrder.testing.table77.orderby(['ID'], jOrder.asc, { indexName: 'id' });
 		});
-	}, { isreference: true });
+		jOB.test("Row by row iteration", function () {
+			return jOrder.shallow(jOrder.testing.table77.flat()).sort(function (a, b) {
+				return a.ID > b.ID ? 1 : a.ID < b.ID ? -1 : 0;
+			});
+		}, { isreference: true });
+		
+		// Sorting on 1000 rows
+		jOB.benchmark("Sorting by 'name'");
+		jOB.test("jOrder.table.orderby()", function () {
+			return jOrder.testing.table1000.orderby(['name'], jOrder.asc, { indexName: 'name' });
+		}, { lengthonly: true });	
+		jOB.test("Row by row iteration", function () {
+			return jOrder.shallow(jOrder.testing.table1000.flat()).sort(function (a, b) {
+				return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+			});
+		}, { isreference: true, lengthonly: true });
 	
-	// Sorting on 1000 rows
-
-	register_benchmark('large', 'sorting1000', "jOrder.table.orderby()", function ()
-	{
-		return jOrder.testing.table1000.orderby(['name'], jOrder.asc, { indexName: 'name' });
-	}, { lengthonly: true });	
-	
-	register_benchmark('large', 'sorting1000', "Row by row iteration", function ()
-	{
-		return jOrder.shallow(jOrder.testing.table1000.flat()).sort(function (a, b)
-		{
-			return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+		// Sorting on 1000 rows, limited
+		jOB.benchmark("Sorting by 'name' (first 20 hits)");
+		jOB.test("jOrder.table.orderby()", function () {
+			return jOrder.testing.table1000.orderby(['name'], jOrder.asc, { indexName: 'name', offset: 0, limit: 20 });
+		});	
+		
+		// Grouping
+		jOB.benchmark("Grouping by 'GroupID'");
+		jOB.test("Summing on field 'Total'", function () {
+			function iterate(aggregated, next) {
+				aggregated.Total += next.Total;
+				return aggregated;
+			}
+			var summed,
+					i;
+			for (i = 0; i < jOB.count; i++) {
+				summed = jOrder.values(jOrder.testing.table77.aggregate('group', null, iterate));
+			}
+			return summed;
 		});
-	}, { isreference: true, lengthonly: true });
-
-	// Sorting on 1000 rows, limited
+		
+		jOrder.logging = false;
 	
-	register_benchmark('large', 'sorting_page1000', "jOrder.table.orderby()", function ()
-	{
-		return jOrder.testing.table1000.orderby(['name'], jOrder.asc, { indexName: 'name', offset: 0, limit: 20 });
-	});	
-	
-	// Grouping
-	
-	register_benchmark('small', 'aggregate77', "Summing on field 'Total'", function ()
-	{
-		function init(next)
-		{
-			next.Total = 0;
-			return next;
-		}
-	
-		function iterate(aggregated, next)
-		{
-			aggregated.Total += next.Total;
-			return aggregated;
-		}
-
-		var summed;
-		for (var i = 0; i < cycles; i++) {
-			summed = jOrder.testing.table77.aggregate('group', init, iterate);
-		}
-		return summed;
+		jOB.start();
 	});
-	
-	jOrder.logging = false;
-});
+}(jQuery,
+	jOrder,
+	jOB));
 
---
+++ test/job/job.js
@@ -0,0 +1,216 @@
+////////////////////////////////////////////////////////////////////////////////
+// jOrder Benchmark
+// Simple JS code benchmarking framework
+// Requires jQuery
+////////////////////////////////////////////////////////////////////////////////
+/*global jQuery */
+
+var jOB = function ($) {
+	var benchmarks = [],	// all benchmarks
+			self;
+
+	// processes a row
+	function process(row, handler) {
+		var result = [],
+				field;
+		for (field in row) {
+			if (row.hasOwnProperty(field)) {
+				result.push(handler(field, row[field]));
+			}
+		}
+		return result.join('');
+	}
+
+	// 'process' handler for data cell
+	function cellHandler(field, value) {
+		return '<td>' + value + '</td>';
+	}
+
+	// adding toolbar
+	$(function () {
+		$(['<label for="job-count">Test cycles: </label>',
+			'<select id="job-count">',
+			'<option value="1">1</option>',
+			'<option value="10" selected="selected">10</option>',
+			'<option value="100">100</option>',
+			'<option value="1000">1000</option>',
+			'</select>',
+			'<label for="job-timeout">Timeout: </label>',
+			'<select id="job-timeout">',
+			'<option value="1000" selected="selected">1 sec</option>',
+			'<option value="5000">5 secs</option>',
+			'</select>',
+			'<input id="job-estimate" type="checkbox" checked="checked" />',
+			'<label for="job-estimate">Estimate on timeout</label>',
+			'</div>'
+		].join(''))
+			.appendTo($('#job-toolbar'));
+			
+		// adding toolbar events
+		$('#job-count').change(function () {
+			self.count = $(this).val();
+		});
+		$('#job-timeout').change(function () {
+			self.timeout = $(this).val();
+		});
+		$('#job-estimate').click(function () {
+			self.estimate = $(this).is(':checked');
+		});
+	});
+	
+	self = {
+		// properties
+		count: 10,				// number of times to run at once
+		timeout: 1000,		// test timeout in ms
+		estimate: true,		// whether to project timed out test durations
+		
+		// registers a benchmark
+		benchmark: function (desc) {
+			benchmarks.push({desc: desc, tests: []});
+		},
+		
+		// runs a test and measures time
+		// -message to be displayed for test
+		// -handler: test to run 'count' times
+		//	expected to return a json table
+		test: function (message, handler, options) {
+			var benchmark = benchmarks[benchmarks.length - 1];
+			if (benchmark) {
+				benchmark.tests.push({message: message, handler: handler, options: options});
+			}
+		},
+
+		// builds a table with result data
+		build: function (json) {
+			$('#job-results')
+				.empty()
+				.append([
+					'<colgroup>',			
+					process(json[0], function (field, value) {
+						return '<col class="' + field + '"></col>';
+					}),
+					'</colgroup>',
+					'<thead>',
+					'<tr>',
+					process(json[0], function (field, value) {
+						return '<th>' + field + '</th>';
+					}),				
+					'</tr>',
+					'</thead>',
+					'<tbody>',
+					function () {
+						var result = [],
+								i;
+						for (i = 0; i < json.length; i++) {
+							result.push('<tr>' + process(json[i], cellHandler) + '</tr>');
+						}
+						return result.join('');
+					}(),
+					'</tbody>'
+				].join(''));
+		},
+
+		// builds a table containing 		
+		start: function () {
+			$('#job-benchmarks').append([
+				'<colgroup>',
+				'<col class="job-desc">',
+				'<col class="job-button">',
+				'<col class="job-result">',
+				'<col class="job-arrow">',
+				'</colgroup>',
+				(function () {
+					var result = [],
+							j, benchmark,
+							i, test;
+					for (j = 0; j < benchmarks.length; j++) {
+						benchmark = benchmarks[j];
+						// benchmark header
+						result.push([
+							'<tbody>',
+							'<tr>',
+							'<th colspan="3">', benchmark.desc, '</th>',
+							'</tr>'
+						].join(''));
+						// tests
+						for (i = 0; i < benchmark.tests.length; i++) {
+							test = benchmark.tests[i];
+							result.push([
+								'<tr id="job-', j, '-', i, '">',
+								'<td>', test.message, '</td>',
+								'<td><input class="job-run" type="button" value="&#8594;" /></td>',
+								'<td class="job-result"></td>',
+								'<td class="job-arrow"></td>',
+								'</tr>'
+							].join(''));
+						}
+						result.push('</tbody>');
+					}
+					return result.join('');
+				}()),
+				'</tbody>'
+			].join(''));
+		}
+	};
+	
+	// events
+	$('input.job-run').live('click', function () {
+		var $this = $(this),
+				$tr = $this.closest('tr'),
+				$tbody = $this.closest('tbody'),
+				id = $tr.attr('id').split('-'),
+				test = benchmarks[id[1]].tests[id[2]],
+				i,
+				start = new Date(), end,
+				unit = 'ms',
+				result;
+	
+		// running test function
+		for (i = 0; i < self.count; i++) {
+			result = test.handler();
+			if (self.timeout < new Date() - start) {
+				break;
+			}
+		}
+		end = new Date();
+		
+		// removing all arrows
+		$('td.job-arrow')
+			.empty();
+				
+		// handling timeout
+		if (i < self.count) {
+			// adding timeout value (% or estimation)
+			$tr
+				.find('.job-result')
+					.text(self.estimate ?
+						String(Math.floor(self.timeout * self.count / i)) + unit :
+						"timeout (" + Math.floor(100 * i / self.count) + "%)")
+				.end();
+
+			// hiding result table
+			$('#job-results')
+				.hide();
+			return;
+		} else {
+			// adding duration and arrow
+			$tr
+				.find('.job-result')
+					.text(String(end - start) + unit)
+				.end()
+				.find('.job-arrow')
+					.html('<span>&#8594;</span>')
+				.end();
+				
+			// aligning result table to benchmark header
+			$('#job-results')
+				.css('top', $tbody.offset().top);		
+
+			// building result table
+			self.build(test.options && test.options.lengthonly ? [{ length: result.length }] : result);
+		}
+	});
+	
+	return self;
+}(jQuery);
+
