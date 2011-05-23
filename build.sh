1679bb4ba2e34ef936fec66480999f3e7d7b67a2
Dan Stocker <dan@kwaia.com>
2011-05-23 09:01:38 +0200

    simple build process

---
+++ build.sh
@@ -0,0 +1,15 @@
+cat \
+src/core/create.js \
+src/core/core.js \
+src/core/constants.js \
+src/core/logging.js \
+src/core/collection.js \
+src/index/signature.js \
+src/index/lookup.js \
+src/index/order.js \
+src/index/index.js \
+src/table/indexes.js \
+src/table/selectors.js \
+src/table/table.js \
+> jorder.js
+
