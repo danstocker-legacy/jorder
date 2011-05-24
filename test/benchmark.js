58e50caaf5b673dc2a5f104b2ee0ab995f4e85ba
Dan Stocker <dan@kwaia.com>
2011-05-24 02:01:36 +0200

    added jLinq and db.js to benchmarks, removed grouping

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,128 +1,246 @@
 ////////////////////////////////////////////////////////////////////////////////
 // jOrder performance tests
 ////////////////////////////////////////////////////////////////////////////////
-/*global jQuery, jOrder, jOB */
+/*global jQuery, jOrder, jOB, DB, jLinq */
 
 // registering benchmarks on document ready
 (function ($, jOrder, jOB) {
+	var
+	
+	// jOrder
+	jorder77 = jOrder.testing.table77,
+	jorder1000 = jOrder.testing.table1000,
+	
+	// db.js
+	db77 = DB(jOrder.testing.json77),
+	db1000 = DB(jOrder.testing.json1000);
+		
 	// Exact search on 77 rows
-	jOB.benchmark("Searching for exact matches ('GroupID' being either 107 or 185)");
-	jOB.test("jOrder.table.where()", function () {
-		return jOrder.testing.table77.where([{ 'GroupID': 107 }, { 'GroupID': 185 }], { renumber: true });
+	jOB.benchmark("Exact mathces on small table", "jOrder", "native", "db.js", "jLinq");
+	jOB.test("'GroupID' being either 107 or 185", function () {
+		return jorder77.where([{ 'GroupID': 107 }, { 'GroupID': 185 }], { renumber: true });
+	}, function () {
+		var result = [], i, row;
+		for (i = 0; i < jOrder.testing.json77.length; i++) {
+			row = jOrder.testing.json77[i];
+			if (row.GroupID === 107 || row.GroupID === 185) {
+				result.push(row);
+			}
+		}
+		return result;
+	}, function () {
+		return db77
+			.find({'GroupID': db77.isin(['107', '185'])})
+			.select('*');
+	}, function () {
+		return jLinq.from(jOrder.testing.json77)
+			.equals('GroupID', 107).or(185)
+			.select();
 	});
-	jOB.test("Row by row iteration", function () {
-		return jOrder.testing.table77n.filter(function (row) {
-			return row.GroupID === 107 || row.GroupID === 185;
-		}, {renumber: true});
-	}, { isreference: true });
 
-	// Exact search on composite index search
-	jOB.benchmark("Searching for exact matches (where 'Total' = 8 and 'Currency' = 'USD')");
-	jOB.test("jOrder.table.where()", function () {
-		return jOrder.testing.table77.where([{ 'Currency': 'USD', 'Total': 8 }], { renumber: true });
+	// Exact search on composite index
+	jOB.test("where 'Total' = 8 and 'Currency' = 'USD'", function () {
+		return jorder77.where([{ 'Currency': 'USD', 'Total': 8 }], { renumber: true });
+	}, function () {
+		var result = [], i, row;
+		for (i = 0; i < jOrder.testing.json77.length; i++) {
+			row = jOrder.testing.json77[i];
+			if (row.Currency === 'USD' && row.Total === 8) {
+				result.push(row);
+			}
+		}
+		return result;		
+	}, function () {
+		return db77
+			.find({'Currency': 'USD', 'Total': 8})
+			.select('*');
+	}, function () {
+		return jLinq.from(jOrder.testing.json77)
+			.equals('Total', 8)
+			.and('Currency', 'USD')
+			.select();
 	});
-	jOB.test("Row by row iteration", function () {
-		return jOrder.testing.table77n.filter(function (row) {
-			return row.Currency === 'USD' && row.Total === 8;
-		}, {renumber: true});
-	}, { isreference: true });
-
-	// Exact search on 1000 rows
-	jOB.benchmark("Searching for exact matches ('id' being either 107 or 115)");
-	jOB.test("jOrder.table.where()", function () {
-		return jOrder.testing.table1000.where([{ 'id': 107 }, { 'id': 115 }], {renumber: true});
-	});
-	jOB.test("Row by row iteration", function () {
-		return jOrder.testing.table1000n.filter(function (row) {
-			return row.id === 107 || row.id === 115;
-		}, {renumber: true});
-	}, { isreference: true });
 
 	// Range search on 77 rows
-	jOB.benchmark("Range search ('Total' between 11 and 15)");	
-	jOB.test("jOrder.table.where()", function () {
+	jOB.benchmark("Range search on small table", "jOrder", "native", "db.js", "jLinq");	
+	jOB.test("'Total' between 11 and 15", function () {
 		return jOrder.testing.table77.where([{ 'Total': { lower: 11, upper: 15 } }], { mode: jOrder.range, renumber: true });
+	}, function () {
+		var result = [], i, row;
+		for (i = 0; i < jOrder.testing.json77.length; i++) {
+			row = jOrder.testing.json77[i];
+			if (row.Total >= 11 && row.Total <= 15) {
+				result.push(row);
+			}
+		}
+		return result;
+	}, function () {
+		return db77
+			.find(db77('Total', '>= 11'))
+			.select('*');
+	}, function () {
+		return jLinq.from(jOrder.testing.json77)
+			.betweenEquals('Total', 11, 15)
+			.select();
 	});
-	jOB.test("Row by row iteration", function () {
-		return jOrder.testing.table77n.filter(function (row) {
-			return row.Total >= 11 && row.Total <= 15;
-		}, {renumber: true});
-	}, { isreference: true });
-	
+
+	// Sorting on 77 rows
+	jOB.benchmark("Sorting on small table", "jOrder", "native", "db.js", "jLinq");
+	jOB.test("by 'ID'", function () {
+		return jorder77.orderby(['ID'], jOrder.asc, { indexName: 'id' });
+	}, function () {
+		return jOrder.shallow(jOrder.testing.json77).sort(function (a, b) {
+			return a.ID > b.ID ? 1 : a.ID < b.ID ? -1 : 0;
+		});
+	}, function () {
+		return db77
+			.order('ID', 'asc')
+			.select('*');
+	}, function () {
+		return jLinq.from(jOrder.testing.json77)
+			.sort('ID')
+			.select();
+	});
+		
+	// Exact search on 1000 rows
+	jOB.benchmark("Exact mathces on big table", "jOrder", "native", "db.js", "jLinq");
+	jOB.test("'id' being either 107 or 115", function () {
+		return jorder1000.where([{ 'id': 107 }, { 'id': 115 }], {renumber: true});
+	}, function () {
+		var result = [], i, row;
+		for (i = 0; i < jOrder.testing.json1000.length; i++) {
+			row = jOrder.testing.json1000[i];
+			if (row.id === 107 || row.id === 115) {
+				result.push(row);
+			}
+		}
+		return result;
+	}, function () {
+		return db1000
+			.find({'id': db77.isin([107, 115])})
+			.select('*');
+	}, function () {
+		return jLinq.from(jOrder.testing.json1000)
+			.equals('id', 107).or(115)
+			.select();
+	});
+
 	// Range search on 1000 rows
-	jOB.benchmark("Range search ('id' between 203 and 315)");
-	jOB.test("jOrder.table.where()", function () {
-		return jOrder.testing.table1000.where([{ 'id': { lower: 203, upper: 315 } }], { mode: jOrder.range, renumber: true, limit: 1000 });
+	jOB.benchmark("Range search on big table", "jOrder", "native", "db.js", "jLinq");
+	jOB.test("'id' between 203 and 315", function () {
+		return jorder1000.where([{ 'id': { lower: 203, upper: 315 } }], { mode: jOrder.range, renumber: true, limit: 1000 });
+	}, function () {
+		var result = [], i, row;
+		for (i = 0; i < jOrder.testing.json1000.length; i++) {
+			row = jOrder.testing.json1000[i];
+			if (row.id >= 203 && row.id <= 315) {
+				result.push(row);
+			}
+		}
+		return result;
+	}, function () {
+		return db1000
+			.find(db1000('id', '>= 203'))
+			.select('*');
+	}, function () {
+		return jLinq.from(jOrder.testing.json1000)
+			.betweenEquals('id', 203, 315)
+			.select();
 	});
-	jOB.test("Row by row iteration", function () {
-		return jOrder.testing.table1000n.filter(function (row) {
-			return row.id >= 203 && row.id <= 315;
-		}, {renumber: true});
-	}, { isreference: true });
 
 	// Range search on 1000 rows with limit
-	jOB.benchmark("Range search with page result ('id' between 203 and 315; hits #20 to #40)");
-	jOB.test("jOrder.table.where()", function () {
-		return jOrder.testing.table1000.where([{ 'id': { lower: 203, upper: 315 } }], {
+	jOB.test("'id' between 203 and 315; hits #20 to #40", function () {
+		return jorder1000.where([{ 'id': { lower: 203, upper: 315 } }], {
 			mode: jOrder.range,
 			renumber: true,
 			offset: 20,
 			limit: 20
 		}, {renumber: true});
+	}, function () {
+		var result = [], i, row, counter = 0;
+		for (i = 0; i < jOrder.testing.json1000.length; i++) {
+			row = jOrder.testing.json1000[i];
+			if (row.id >= 203 && row.id <= 315) {
+				if (counter > 40) {
+					break;
+				} else if (counter < 20) {
+					counter++;
+					continue;
+				} else {
+					counter++;
+					result.push(row);
+				}
+			}
+		}
+		return result;
+	}, function () {
+		return db1000
+			.find(db1000('id', '>= 203'))
+			.select('*')
+			.slice(20, 40);
+	}, function () {
+		return jLinq.from(jOrder.testing.json1000)
+			.betweenEquals('id', 203, 315)
+			.skipTake(20, 20);
 	});
 
-	// Freetext search on 1000 rows
-	jOB.benchmark("Free text search (rows with 'name' field starting with \"con\")");
-	jOB.test("jOrder.table.where()", function () {
-		return jOrder.testing.table1000.where([{ 'name': 'con' }], { mode: jOrder.startof, indexName: 'fulltext', limit: 1000, renumber: true });
-	});
-	jOB.test("Row by row iteration", function () {
-		return jOrder.testing.table1000n.filter(function (row) {
-			return null !== row.name.match(/\bcon/i);
-		}, {renumber: true});
-	}, { isreference: true });
-	
-	// Sorting on 77 rows
-	jOB.benchmark("Sorting by 'ID'");
-	jOB.test("jOrder.table.orderby()", function () {
-		return jOrder.testing.table77.orderby(['ID'], jOrder.asc, { indexName: 'id' });
-	});
-	jOB.test("Row by row iteration", function () {
-		return jOrder.shallow(jOrder.testing.table77.flat()).sort(function (a, b) {
-			return a.ID > b.ID ? 1 : a.ID < b.ID ? -1 : 0;
-		});
-	}, { isreference: true });
-	
 	// Sorting on 1000 rows
-	jOB.benchmark("Sorting by 'name'");
-	jOB.test("jOrder.table.orderby()", function () {
-		return jOrder.testing.table1000.orderby(['name'], jOrder.asc, { indexName: 'name' });
-	}, { lengthonly: true });	
-	jOB.test("Row by row iteration", function () {
-		return jOrder.shallow(jOrder.testing.table1000.flat()).sort(function (a, b) {
+	jOB.benchmark("Sorting on big table", "jOrder", "native", "db.js", "jLinq");
+	jOB.test("by 'name'", function () {
+		return jorder1000.orderby(['name'], jOrder.asc, { indexName: 'name' });
+	}, function () {
+		return jOrder.shallow(jOrder.testing.json1000).sort(function (a, b) {
 			return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
 		});
-	}, { isreference: true, lengthonly: true });
+	}, function () {
+		return db1000
+			.order('name', 'asc')
+			.select('*');
+	}, function () {
+		return jLinq.from(jOrder.testing.json1000)
+			.sort('name')
+			.select();
+	}, { lengthonly: true });
 
 	// Sorting on 1000 rows, limited
-	jOB.benchmark("Sorting by 'name' (first 20 hits)");
-	jOB.test("jOrder.table.orderby()", function () {
-		return jOrder.testing.table1000.orderby(['name'], jOrder.asc, { indexName: 'name', offset: 0, limit: 20 });
-	});	
+	jOB.test("by 'name' (first 20 hits)", function () {
+		return jorder1000.orderby(['name'], jOrder.asc, { indexName: 'name', offset: 0, limit: 20 });
+	}, function () {
+		return jOrder.shallow(jOrder.testing.json1000).sort(function (a, b) {
+			return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+		}).slice(0, 20);
+	}, function () {
+		return db1000
+			.order('name', 'asc')
+			.select('*')
+			.slice(0, 20);
+	}, function () {
+		return jLinq.from(jOrder.testing.json1000)
+			.sort('name')
+			.take(20);
+	});
 	
-	// Grouping
-	jOB.benchmark("Grouping by 'GroupID'");
-	jOB.test("Summing on field 'Total'", function () {
-		function iterate(aggregated, next) {
-			aggregated.Total += next.Total;
-			return aggregated;
+	// Freetext search on 1000 rows
+	jOB.benchmark("Free text search on large table", "jOrder", "native", "db.js", "jLinq");
+	jOB.test("rows with 'name' field starting with \"con\"", function () {
+		return jorder1000.where([{ 'name': 'con' }], { mode: jOrder.startof, indexName: 'fulltext', limit: 1000, renumber: true });
+	}, function () {
+		var result = [], i, row;
+		for (i = 0; i < jOrder.testing.json1000.length; i++) {
+			row = jOrder.testing.json1000[i];
+			if (null !== row.name.match(/\bcon/i)) {
+				result.push(row);
+			}
 		}
-		var summed,
-				i;
-		for (i = 0; i < jOB.count; i++) {
-			summed = jOrder.values(jOrder.testing.table77.aggregate('group', null, iterate));
-		}
-		return summed;
+		return result;		
+	}, function () {
+		return db1000
+			.find({'name': db1000.like('con')})
+			.select('*');
+	}, function () {
+		return jLinq.from(jOrder.testing.json1000)
+			.starts('name', 'con')
+			.select();
 	});
 	
 	jOrder.logging = false;
