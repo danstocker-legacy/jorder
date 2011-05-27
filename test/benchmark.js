91b545fdf9a4bafd0d3ed6fe8d0d220063b4dd64
Dan Stocker <dan@kwaia.com>
2011-05-27 19:13:13 +0200

    taffy db added as candidate + various fixes

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 // jOrder performance tests
 ////////////////////////////////////////////////////////////////////////////////
-/*global jQuery, jOrder, jOB, DB, jLinq */
+/*global jQuery, jOrder, jOB, DB, jLinq, TAFFY */
 
 // registering benchmarks on document ready
 (function ($, jOrder, jOB) {
@@ -13,10 +13,17 @@
 	
 	// db.js
 	db77 = DB(jOrder.testing.json77),
-	db1000 = DB(jOrder.testing.json1000);
-		
+	db1000 = DB(jOrder.testing.json1000),
+	
+	// TAFFY
+	// Taffy modofies the original json
+	taffy77j,
+	taffy77 = new TAFFY(jOrder.testing.json77),
+	taffy1000j,
+	taffy1000 = new TAFFY(jOrder.testing.json1000);
+	
 	// Exact search on 77 rows
-	jOB.benchmark("Search on small table", "jOrder", "native", "db.js", "jLinq");
+	jOB.benchmark("Search on small table", "jOrder 1.2", "native", "db.js", "jLinq 3.0.1", "Taffy DB 1.7.3");
 	jOB.test("'GroupID' being either 107 or 185", function () {
 		return jorder77.where([{ 'GroupID': 107 }, { 'GroupID': 185 }], { renumber: true });
 	}, function () {
@@ -36,6 +43,9 @@
 		return jLinq.from(jOrder.testing.json77)
 			.equals('GroupID', 107).or(185)
 			.select();
+	}, function () {
+		return taffy77
+			.get({'GroupID': ['107', '185']});
 	});
 
 	// Exact search on composite index
@@ -59,11 +69,14 @@
 			.equals('Total', 8)
 			.and('Currency', 'USD')
 			.select();
+	}, function () {
+		return taffy77
+			.get({'Total': 8, 'Currency': 'USD'});
 	});
 
 	// Range search on 77 rows
 	jOB.test("'Total' between 11 and 15", function () {
-		return jorder77.where([{ 'Total': { lower: 11, upper: 15 } }], { mode: jOrder.range, renumber: true });
+		return jorder77.where([{ 'Total': { lower: 10.9, upper: 15.1 } }], { mode: jOrder.range, renumber: true });
 	}, function () {
 		var result = [], i, row;
 		for (i = 0; i < jOrder.testing.json77.length; i++) {
@@ -75,16 +88,19 @@
 		return result;
 	}, function () {
 		return db77
-			.find(db77('Total', '>= 11'))
+			.find(db77('Total', '>= 11'), db77('Total', '<= 15'))
 			.select('*');
 	}, function () {
 		return jLinq.from(jOrder.testing.json77)
 			.betweenEquals('Total', 11, 15)
 			.select();
+	}, function () {
+		var pass1 = taffy77.get({'Total': {gte: 11}});
+		return new TAFFY(pass1).get({'Total': {lte: 15}});
 	});
 
 	// Sorting on 77 rows
-	jOB.benchmark("Sorting on small table", "jOrder", "native", "db.js", "jLinq");
+	jOB.benchmark("Sorting on small table", "jOrder 1.2", "native", "db.js", "jLinq 3.0.1", "Taffy DB 1.7.3");
 	jOB.test("by 'ID'", function () {
 		return jorder77.orderby(['ID'], jOrder.asc, { indexName: 'id' });
 	}, function () {
@@ -99,10 +115,15 @@
 		return jLinq.from(jOrder.testing.json77)
 			.sort('ID')
 			.select();
+	}, function () {
+		taffy77j = jOrder.shallow(jOrder.testing.json77);
+		(new TAFFY(taffy77j))
+			.orderBy({'ID': 'asc'});
+		return taffy77j;
 	});	
 
 	// Exact search on 1000 rows
-	jOB.benchmark("Search on big table", "jOrder", "native", "db.js", "jLinq");
+	jOB.benchmark("Search on big table", "jOrder 1.2", "native", "db.js", "jLinq 3.0.1", "Taffy DB 1.7.3");
 	jOB.test("'id' being either 107 or 115", function () {
 		return jorder1000.where([{ 'id': 107 }, { 'id': 115 }], {renumber: true});
 	}, function () {
@@ -122,11 +143,14 @@
 		return jLinq.from(jOrder.testing.json1000)
 			.equals('id', 107).or(115)
 			.select();
+	}, function () {
+		return taffy1000
+			.get({id: [107, 115]});
 	});
 
 	// Range search on 1000 rows
 	jOB.test("'id' between 203 and 315", function () {
-		return jorder1000.where([{ 'id': { lower: 203, upper: 315 } }], { mode: jOrder.range, renumber: true, limit: 1000 });
+		return jorder1000.where([{ 'id': { lower: 202.9, upper: 315.1 } }], { mode: jOrder.range, renumber: true, limit: 1000 });
 	}, function () {
 		var result = [], i, row;
 		for (i = 0; i < jOrder.testing.json1000.length; i++) {
@@ -138,12 +162,15 @@
 		return result;
 	}, function () {
 		return db1000
-			.find(db1000('id', '>= 203'))
+			.find(db1000('id', '>= 203'), db1000('id', '<= 315'))
 			.select('*');
 	}, function () {
 		return jLinq.from(jOrder.testing.json1000)
 			.betweenEquals('id', 203, 315)
 			.select();
+	}, function () {
+		var pass1 = taffy1000.get({id: {gte: 203}});
+		return new TAFFY(pass1).get({id: {lte: 315}});
 	});
 
 	// Range search on 1000 rows with limit
@@ -180,6 +207,11 @@
 		return jLinq.from(jOrder.testing.json1000)
 			.betweenEquals('id', 203, 315)
 			.skipTake(20, 20);
+	}, function () {
+		var pass1 = taffy1000.get({id: {gte: 203}});
+		return new TAFFY(pass1)
+			.get({id: {lte: 315}})
+			.slice(20, 40);
 	});
 	
 	// Freetext search on 1000 rows
@@ -196,16 +228,21 @@
 		return result;		
 	}, function () {
 		return db1000
-			.find({'name': db1000.like('con')})
+			.find({'name': function (value) {
+				return null !== value.match(/\bcon/i);
+			}})
 			.select('*');
 	}, function () {
 		return jLinq.from(jOrder.testing.json1000)
-			.starts('name', 'con')
+			.match('name', /\bcon/i)
 			.select();
+	}, function () {
+		return taffy1000
+			.get({'name': {regex: /\bcon/i}});
 	});
 
 	// Sorting on 1000 rows
-	jOB.benchmark("Sorting on big table", "jOrder", "native", "db.js", "jLinq");
+	jOB.benchmark("Sorting on big table", "jOrder 1.2", "native", "db.js", "jLinq 3.0.1", "Taffy DB 1.7.3");
 	jOB.test("by 'name'", function () {
 		return jorder1000.orderby(['name'], jOrder.asc, { indexName: 'name' });
 	}, function () {
@@ -214,12 +251,17 @@
 		});
 	}, function () {
 		return db1000
-			.order('name', 'asc')
+			.order('name')
 			.select('*');
 	}, function () {
 		return jLinq.from(jOrder.shallow(jOrder.testing.json1000))
 			.sort('name')
 			.select();
+	}, function () {
+		taffy1000j = jOrder.shallow(jOrder.testing.json1000);
+		(new TAFFY(taffy1000j))
+			.orderBy({'name': 'asc'});
+		return taffy1000j;
 	}, { lengthonly: true });
 
 	// Sorting on 1000 rows, limited
@@ -238,6 +280,12 @@
 		return jLinq.from(jOrder.shallow(jOrder.testing.json1000))
 			.sort('name')
 			.take(20);
+	}, function () {
+		taffy1000j = jOrder.shallow(jOrder.testing.json1000);
+		(new TAFFY(taffy1000j))
+			.orderBy({'name': 'asc'});
+		return taffy1000j
+			.slice(0, 20);
 	});
 	
 	jOrder.logging = false;
