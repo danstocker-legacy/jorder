2081f98ac4f8b88925819b2feeffad45b4bca3d9
Dan Stocker <dan@kwaia.com>
2011-06-08 09:12:05 +0200

    Sys.Debug ditched

--- src/core/logging.js
+++ src/core/logging.js
@@ -1,41 +1,23 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Logging
 ////////////////////////////////////////////////////////////////////////////////
-/*global jOrder, window, Sys */
+/*global jOrder, window, console */
 
 jOrder.logging = function ($core) {
-	// utility functions
-	var self,
-			log;
-
-	if (!window.console) {
-		log = window.Sys && window.Sys.Debug ?
-			// using Sys.Debug
-			function (msg) {
-				Sys.Debug.trace(msg);
-			} :
-			// no logging
-			function () {};
-
-		window.console = {
-			log: log,
-			warn: log,
-			error: log
-		};
-	}
+	var lconsole = typeof window === 'object' ? window.console : console,
 	
 	// properties
 	self = {
 		// logs to console
 		log: function (msg) {
-			if (jOrder.logging) {
-				window.console.log(msg);
+			if (lconsole && jOrder.logging) {
+				lconsole.log(msg);
 			}
 		},
 		// issues a warning
 		warn: function (msg) {
-			if (jOrder.logging) {
-				window.console.warn(msg);
+			if (lconsole && jOrder.logging) {
+				lconsole.warn(msg);
 			}
 		},
 		// use throw instead of console.error()
