b18608a51a763f94e816420274e41dbffb9b5a84
Dan Stocker <dan@kwaia.com>
2013-04-30 10:08:29 +0200

    namespace file for using libs

---
+++ js/namespace.js
@@ -0,0 +1,9 @@
+/**
+ * Top-Level Library Namespace
+ */
+/*global require */
+/** @namespace */
+var jorder = {},
+    dessert = dessert || require('dessert').dessert,
+    troop = troop || require('troop').troop,
+    sntls = sntls || require('sntls').sntls;
