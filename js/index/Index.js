86bbd9902889fbd93e37579f4ecb4ae4f4889f5c
Dan Stocker <dan@kwaia.com>
2013-05-12 12:16:11 +0200

    jorder.Index with basic content manipulation and lookups

---
+++ js/index/Index.js
@@ -0,0 +1,116 @@
+/**
+ * Datastore Index
+ */
+/*global dessert, troop, sntls, jorder */
+troop.promise(jorder, 'Index', function () {
+    "use strict";
+
+    /**
+     * @class jorder.Index
+     * @extends troop.Base
+     */
+    jorder.Index = troop.Base.extend()
+        .addMethod(/** @lends jorder.Index */{
+            /**
+             * @name jorder.Index.create
+             * @return {jorder.Index}
+             */
+
+            /**
+             * @param {string[]} fieldNames Field names
+             * @param {number} [signatureType='string'] Signature type, see SIGNATURE_TYPES
+             */
+            init: function (fieldNames, signatureType) {
+                /**
+                 * Row signature associated with index.
+                 * Provides validation and index key generation.
+                 * @type {jorder.RowSignature}
+                 */
+                this.rowSignature = jorder.RowSignature.create(fieldNames, signatureType);
+
+                /**
+                 * Holds index key -> row ID associations.
+                 * One index key may reference more than one row IDs.
+                 * @type {sntls.StringDictionary}
+                 */
+                this.lookup = sntls.StringDictionary.create();
+
+                /**
+                 * Holds index keys in ascending order. (With multiplicity)
+                 * TODO: investigate no-multiplicity solution
+                 * @type {sntls.OrderedList}
+                 */
+                this.order = sntls.OrderedStringList.create();
+            },
+
+            /**
+             * Adds single row to index.
+             * @param {object} row Table row
+             * @param {string} rowId Row ID: original index of row in table
+             * @return {jorder.Index}
+             */
+            addRow: function (row, rowId) {
+                // calculating index keys based on row
+                var keys = this.rowSignature.getKeysForRow(row);
+
+                // adding key / rowId pairs to lookup index
+                this.lookup.addItems(keys, rowId);
+
+                // adding keys to ordered index (w/ multiplicity)
+                this.order.addItems(keys);
+
+                return this;
+            },
+
+            /**
+             * Removes single row from index.
+             * @param {object} row Table row
+             * @param {string} rowId Row ID: original index of row in table
+             * @return {jorder.Index}
+             */
+            removeRow: function (row, rowId) {
+                // calculating index keys based on row
+                var keys = this.rowSignature.getKeysForRow(row);
+
+                // removing key / rowId pairs from lookup index
+                this.lookup.removeItems(keys, rowId);
+
+                // removing keys from ordered index (w/ multiplicity)
+                this.order.removeItems(keys);
+
+                return this;
+            },
+
+            /**
+             * Retrieves a list of row ids associated with the specified keys.
+             * @param {string[]|number[]} keys
+             * @return {string[]}
+             */
+            getRowIdsForKeys: function (keys) {
+                return sntls.StringDictionary.create(keys)
+                    .combineWith(this.lookup)// selecting row IDs for specified keys
+                    .reverse()// collapsing unique row IDs
+                    .getKeys();
+            },
+
+            /**
+             * Retrieves a list of unique row IDs matching index values
+             * that fall between the specified bounds.
+             * @param {string|number} startValue Lower index bound
+             * @param {string|number} endValue Upper index bound
+             * @param {number} [offset] Starting position of results
+             * @param {number} [limit]
+             * @return {string[]}
+             */
+            getRowIdsForKeyRange: function (startValue, endValue, offset, limit) {
+                return this.order.getRangeAsHash(startValue, endValue)
+                    .toStringDictionary()
+                    .reverse()// collapsing unique index values
+                    .getKeysAsHash()// getting unique index values in a hash
+                    .toStringDictionary()
+                    .combineWith(this.lookup)// obtaining row IDs from lookup
+                    .reverse()// collapsing unique row IDs
+                    .getKeys();
+            }
+        });
+});
---
+++ js/index/Index.test.js
@@ -0,0 +1,130 @@
+/*global module, test, ok, raises, equal, strictEqual, deepEqual */
+/*global sntls, jorder */
+(function () {
+    "use strict";
+
+    module("Index");
+
+    test("Instantiation", function () {
+        var index = jorder.Index.create(['foo', 'bar'], 'number');
+
+        ok(index.rowSignature.isA(jorder.RowSignature), "Row signature");
+        ok(index.lookup.isA(sntls.Dictionary), "Index lookup");
+        ok(index.order.isA(sntls.OrderedList), "Index order");
+    });
+
+    test("Row addition", function () {
+        var index = jorder.Index.create(['foo', 'bar']);
+
+        raises(function () {
+            index.addRow({invalid: 1}, 0);
+        }, "Row doesn't match signature");
+
+        index.addRow({foo: 5, bar: 7}, 0);
+
+        deepEqual(
+            index.lookup.items,
+            {
+                '5|7': 0
+            },
+            "Lookup after first row"
+        );
+
+        deepEqual(
+            index.order.items,
+            ['5|7'],
+            "Order after first row"
+        );
+    });
+
+    test("Index population", function () {
+        var index = jorder.Index.create(['foo', 'bar'])
+            .addRow({foo: 5, bar: 7}, 0)
+            .addRow({foo: 3, bar: 2}, 1)
+            .addRow({foo: 4, bar: 1}, 2)
+            .addRow({foo: 3, bar: 2}, 3)
+            .addRow({foo: 3, bar: 3}, 4)
+            .addRow({foo: 5, bar: 7}, 5)
+            .addRow({foo: 5, bar: 1}, 6)
+            .addRow({foo: 1, bar: 7}, 7)
+            .addRow({foo: 1, bar: 1}, 8)
+            .addRow({foo: 1, bar: 1}, 9);
+
+        deepEqual(
+            index.lookup.items,
+            {
+                '5|7': [0, 5],
+                '3|2': [1, 3],
+                '4|1': 2,
+                '3|3': 4,
+                '5|1': 6,
+                '1|7': 7,
+                '1|1': [8, 9]
+            },
+            "Populated lookup index"
+        );
+
+        deepEqual(
+            index.order.items,
+            [
+                '5|7',
+                '3|2',
+                '4|1',
+                '3|2',
+                '3|3',
+                '5|7',
+                '5|1',
+                '1|7',
+                '1|1',
+                '1|1'
+            ].sort(),
+            "Populated order index"
+        );
+    });
+
+    test('Lookup', function () {
+        var index = jorder.Index.create(['foo'], 'number')
+            .addRow({foo: 5}, 0)
+            .addRow({foo: 3}, 1)
+            .addRow({foo: 4}, 2)
+            .addRow({foo: 3}, 3)
+            .addRow({foo: 3}, 4)
+            .addRow({foo: 5}, 5)
+            .addRow({foo: 5}, 6)
+            .addRow({foo: 1}, 7)
+            .addRow({foo: 1}, 8)
+            .addRow({foo: 1}, 9);
+
+        deepEqual(
+            index.getRowIdsForKeys(['3', '4']).sort(),
+            ['1', '2', '3', '4'],
+            "Row IDs for index keys 3 and 4"
+        );
+    });
+
+    test("Range retrieval", function () {
+        var index = jorder.Index.create(['foo'], 'number')
+            .addRow({foo: 5}, 0)
+            .addRow({foo: 3}, 1)
+            .addRow({foo: 4}, 2)
+            .addRow({foo: 3}, 3)
+            .addRow({foo: 3}, 4)
+            .addRow({foo: 5}, 5)
+            .addRow({foo: 5}, 6)
+            .addRow({foo: 1}, 7)
+            .addRow({foo: 1}, 8)
+            .addRow({foo: 1}, 9);
+
+        deepEqual(
+            index.getRowIdsForKeyRange(3, 4).sort(),
+            ['1', '3', '4'],
+            "Row IDs between index key 3 and 4 (excl.)"
+        );
+
+        deepEqual(
+            index.getRowIdsForKeyRange(4, 6).sort(),
+            ['0', '2', '5', '6'],
+            "Row IDs between index key 4 and 6 (excl.)"
+        );
+    });
+}());
