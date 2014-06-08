6f8087184f8d31164f69ec5bab4fa521c8c86889
Dan Stocker <dan@kwaia.com>
2014-06-08 10:51:00 +0100

    adds alias (j$)

--- js/namespace.js
+++ js/namespace.js
@@ -4,6 +4,7 @@
 /*global require */
 /** @namespace */
 var jorder = {},
+    j$ = jorder,
     dessert = dessert || require('dessert'),
     troop = troop || require('troop'),
     sntls = sntls || require('sntls');
