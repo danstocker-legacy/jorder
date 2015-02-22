7658e959e0399ec40af99f301b7b8237906f08a3
Dan Stocker <dan@kwaia.com>
2015-02-22 09:32:24 +0000

    refactors IndexCollection tests

--- js/IndexCollection.test.js
+++ js/IndexCollection.test.js
@@ -13,8 +13,8 @@
 
         jorder.RowSignature.addMocks({
             containedByRow: function (row) {
-                strictEqual(row, myRow, "Row passed");
-                strictEqual(this, index.rowSignature, "Index matches");
+                strictEqual(this, index.rowSignature, "should check whether index is contained by row");
+                strictEqual(row, myRow, "should pass row to containment checker");
             }
         });
 
@@ -29,14 +29,16 @@
         equal(
             jorder.IndexCollection._indexFieldCountMapper(index),
             index.rowSignature.fieldNames.length,
-            "Index signature field count"
-        );
+            "should return number of fields involved in index");
     });
 
     test("DESC Numeric comparator", function () {
-        equal(jorder.IndexCollection._descNumericComparator(1, 2), 1, "First is lower");
-        equal(jorder.IndexCollection._descNumericComparator(3, 2), -1, "First is higher");
-        equal(jorder.IndexCollection._descNumericComparator(3, 3), 0, "Equal");
+        equal(jorder.IndexCollection._descNumericComparator(1, 2), 1,
+            "should return 1 when second argument is higher");
+        equal(jorder.IndexCollection._descNumericComparator(3, 2), -1,
+            "should return -1 when second argument is lower");
+        equal(jorder.IndexCollection._descNumericComparator(3, 3), 0,
+            "should return zero when arguments are equal");
     });
 
     test("Index addition", function () {
@@ -47,7 +49,7 @@
 
         deepEqual(indexCollection.items, {
             'foo|bar%string%ascending': index
-        }, "Index added to collection");
+        }, "should add index to items");
     });
 
     test("Index removal", function () {
@@ -67,19 +69,17 @@
     test("Exact index lookup by field names", function () {
         var index1 = jorder.Index.create(['foo', 'bar']),
             index2 = jorder.Index.create(['field1', 'field2']),
-            indexCollection = jorder.IndexCollection.create();
+            indexCollection = jorder.IndexCollection.create()
+                .setItem(index1)
+                .setItem(index2);
 
-        indexCollection
-            .setItem(index1)
-            .setItem(index2);
-
-        equal(typeof indexCollection.getIndexForFields(['foo']), 'undefined', "No matching index");
+        equal(typeof indexCollection.getIndexForFields(['foo']), 'undefined',
+            "should return undefined when no matching index is found");
 
         strictEqual(
             indexCollection.getIndexForFields(['foo', 'bar']),
             index1,
-            "Index retrieved"
-        );
+            "should return matching index");
     });
 
     test("Fetching indexes matching a row", function () {
@@ -93,15 +93,14 @@
         // full match (ie. all index fields are present in row)
         result = indexCollection.getIndexesForRow({foo: 'hello', bar: 'world'});
 
-        ok(result.isA(jorder.IndexCollection), "Return type IndexCollection");
+        ok(result.isA(jorder.IndexCollection), "should return IndexCollection instance");
         deepEqual(
             result.getKeys().sort(),
             ['foo%string%ascending', 'foo|bar%string%ascending'],
-            "Only fully matching indexes returned"
-        );
+            "should return only matching indexes");
     });
 
-    test("Fetching index for row", function () {
+    test("Fetching single index for row", function () {
         var indexCollection = jorder.IndexCollection.create()
                 .setItem(jorder.Index.create(['foo', 'bar']))
                 .setItem(jorder.Index.create(['foo']))
@@ -112,13 +111,12 @@
         // could yield any of the first 3 indexes
         result = indexCollection.getIndexForRow({foo: 'hello', bar: 'world', baz: '!!!'});
 
-        ok(result.isA(jorder.Index), "Return type Index");
+        ok(result.isA(jorder.Index), "should return Index instance");
         ok(
             result.rowSignature.fieldSignature === 'foo|bar%string' ||
             result.rowSignature.fieldSignature === 'foo%string' ||
             result.rowSignature.fieldSignature === 'foo|baz%string',
-            "Result index signature"
-        );
+            "should return either of the matching indexes");
     });
 
     test("Fetching best index for row", function () {
@@ -130,16 +128,18 @@
 
         result = indexCollection.getBestIndexForRow({hello: 'world'});
 
-        equal(typeof result, 'undefined', "No fitting index");
+        equal(typeof result, 'undefined', "should return undefined for no matching index");
 
         // yields the one with the most matching fields
         result = indexCollection.getBestIndexForRow({foo: 'hello', bar: 'world', baz: '!!!'});
 
-        ok(result.isA(jorder.Index), "Return type");
-        equal(result.rowSignature.fieldSignature, 'foo|bar|baz%string', "Result index signature");
+        ok(result.isA(jorder.Index), "should return Index instance");
+        equal(result.rowSignature.fieldSignature, 'foo|bar|baz%string',
+            "should return index with the right row signature");
 
         result = indexCollection.getBestIndexForRow({foo: 'hello', bar: 'world'});
-        equal(result.rowSignature.fieldSignature, 'foo|bar%string', "Result index signature");
+        equal(result.rowSignature.fieldSignature, 'foo|bar%string',
+            "should return index with the right row signature");
     });
 
     test("Fetching best index for multiple fields", function () {
@@ -150,7 +150,8 @@
 
         indexCollection.addMocks({
             getBestIndexForRow: function (row) {
-                deepEqual(row, {foo: '0', 'bar': '1', baz: '2'}, "should pass dummy row to index-by-row getter");
+                deepEqual(row, {foo: '0', 'bar': '1', baz: '2'},
+                    "should pass dummy row to index-by-row getter");
                 return bestIndex;
             }
         });
@@ -159,31 +160,5 @@
             indexCollection.getBestIndexForFields(['foo', 'bar', 'baz']),
             bestIndex,
             "should return index returned by by-row getter");
-    });
-
-    test("Row addition", function () {
-        var index1 = jorder.Index.create(['foo', 'bar']),
-            index2 = jorder.Index.create(['foo', 'moo']),
-            indexCollection = jorder.IndexCollection.create()
-                .setItem(index1)
-                .setItem(index2);
-
-        indexCollection.addRow({foo: 'hello', bar: 'world', moo: 'cow'}, 0);
-
-        deepEqual(
-            index1.rowIdLookup.items,
-            {
-                'hello|world': 0
-            },
-            "First lookup index"
-        );
-
-        deepEqual(
-            index2.rowIdLookup.items,
-            {
-                'hello|cow': 0
-            },
-            "Second lookup index"
-        );
     });
 }());
